6c98d36d2ed8ce43503c4ea331b9c25c774cef91
ppatierno <ppatierno@live.com>
2016-05-05 14:33:48 +0200

    Updated all files to Apache Kafka 0.9.0.1

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ "$1" = 'kafka-server-start.sh' ]; then
 
-    sed -i -r 's|zookeeper.connect=localhost:2181|zookeeper.connect=zookeeper:2181|g' /opt/kafka_2.10-0.8.2.1/config/server.properties
+    sed -i -r 's|zookeeper.connect=localhost:2181|zookeeper.connect=zookeeper:2181|g' /opt/kafka_2.11-0.9.0.1/config/server.properties
 
     exec "$@"
 fi
